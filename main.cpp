93a5933992e18cd16caf31414292fbc209910625
thevoxium <thevoxium@gmail.com>
2024-10-31 02:07:02 +0530

    added second window to display commit info

--- main.cpp
+++ main.cpp
@@ -3,17 +3,30 @@
 #include <ncurses.h>
 #include <git2.h>
 #include <iostream>
-
+#include <ctime> 
+#include <string> 
+#include <vector> 
 
 using namespace std;
 
 #define MAX_ROWS 1000
+
+
+struct CommitInfo{
+  string message;
+  string author;
+  string email;
+  string commit_id;
+  time_t commit_time;
+};
 
 int main(){
   initscr();
   cbreak();
   noecho();
   keypad(stdscr, TRUE);
+  
+  vector<CommitInfo> commitList;
 
   char* commit_message[MAX_ROWS];
 
@@ -34,6 +47,12 @@ int main(){
   mvwprintw(win, 0, 1, "Commit History");
   wrefresh(win);
 
+
+  WINDOW* commit_info_window = newwin(commit_window_size-4, maxX-2, commit_window_size + 4, 1);
+  box(commit_info_window, 0, 0);
+  mvwprintw(commit_info_window, 0, 1, "Commit Info");
+  wrefresh(commit_info_window);
+
   int error = git_repository_open(&repo, "/Users/anshul/interviewer/");
 
   if(error < 0){
@@ -52,7 +71,7 @@ int main(){
   }
 
   git_revwalk_push_head(walker);
-  git_revwalk_sorting(walker, GIT_SORT_TIME | GIT_SORT_REVERSE);
+  git_revwalk_sorting(walker, GIT_SORT_TIME);
 
   git_oid oid;
 
@@ -61,8 +80,14 @@ int main(){
   while(git_revwalk_next(&oid, walker) == 0){
     
     if (git_commit_lookup(&commit, repo, &oid) == 0){
-      const char* message = git_commit_message(commit);
-      commit_message[commit_message_count] = strdup(message);
+      CommitInfo info;
+
+      info.message = git_commit_message(commit);
+      info.author = git_commit_author(commit)->name;
+      info.email = git_commit_author(commit)->email;
+      info.commit_time = git_commit_time(commit);
+      info.commit_id = git_oid_tostr_s(&oid);
+      commitList.push_back(info);
     }
     commit_message_count++;
 
@@ -75,43 +100,48 @@ int main(){
   int lines_to_display = commit_window_size-2;
   
   int cursor_position = 1;
-
+  int commit_info_window_count = 0;
   while(1){
-    
     cursor_position = max(min(cursor_position, commit_window_size-2), 1);
-    starting_line = max(min(starting_line, commit_window_size-2), 0);
+    starting_line = max(min(starting_line, commit_message_count - lines_to_display), 0);
     werase(win);
     box(win, 0, 0);
     mvwprintw(win, 0, 1, "Commit History");
 
     for (int i=starting_line; i<starting_line+lines_to_display && i<commit_message_count; i++){
-      mvwprintw(win, i-starting_line+1, 1, "%s", commit_message[i]);
+      mvwprintw(win, i-starting_line+1, 1, "%s", commitList[i].message.c_str());
       y_coordinate_commit++;
     }
 
     mvwchgat(win, cursor_position, 1, -1, A_REVERSE, 0, NULL);
-
+    mvwprintw(commit_info_window, 1, 1, "%s", commitList[commit_info_window_count].message.c_str());
+    wrefresh(commit_info_window);
     wrefresh(win);
+    
 
     int ch = getch();
-    if (ch == 'q'){
-      break;
-    }else if(ch ==  KEY_DOWN || ch == 'k'){
-      if (starting_line+lines_to_display < commit_message_count){
-        cursor_position++;
-        if(cursor_position > lines_to_display){
-          cursor_position--;
-          starting_line++;
-        } 
-      }
-    }else if (ch == KEY_UP || ch == 'j'){
-      if (cursor_position > 0){
-        cursor_position--;
-        if(cursor_position <= 0){
-          starting_line--;
+    if (ch == 'q') {
+        break;
+    } else if (ch == KEY_DOWN || ch == 'k') {
+        if (commit_info_window_count < commit_message_count - 1) {
+            commit_info_window_count++;
+            if (cursor_position < lines_to_display) {
+                cursor_position++;
+            } else if (starting_line + lines_to_display < commit_message_count) {
+                starting_line++;
+            }
         }
-      } 
+    } else if (ch == KEY_UP || ch == 'j') {
+        if (commit_info_window_count > 0) {
+            commit_info_window_count--;
+            if (cursor_position > 1) {
+                cursor_position--;
+            } else if (starting_line > 0) {
+                starting_line--;
+            }
+        }
     }
+
 
   }
   
